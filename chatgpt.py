d1222ad50f363515a25dab806feafd434843581e
Alexey Krylov <elkurhah@gmail.com>
2023-05-29 14:29:59 +0300

    add authorization

--- chatgpt.py
+++ chatgpt.py
@@ -4,16 +4,15 @@ import openai
 import telebot
 
 
-def readfile(file):
+def read_file(file):
     """ Read file """
     with open(file, "r", encoding="UTF-8") as file1:
         for line in file1:
             return line.strip()
 
 
-TOKEN = readfile("telebotapi.txt")
+TOKEN = read_file("telebotapi.txt")
 bot = telebot.TeleBot(TOKEN)
-
 
 # Configure logging
 logging.basicConfig(
@@ -22,35 +21,52 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+# Authorized users
+AUTHORIZED_USERS = ["@hijacker555", "user2", "user3"]
 
 @bot.message_handler(commands=['start'])
 def start(message):
     """ Start """
-    bot.send_message(chat_id=message.chat.id,
-                     text="Hi, I'm a bot powered by OpenAI. How can I help you today?")
+    username = message.from_user.username
+    if username in AUTHORIZED_USERS:
+        bot.send_message(chat_id=message.chat.id,
+                         text="Hi, I'm a bot powered by OpenAI. How can I help you today?")
+        logger.info(f"User '{username}' authorized and started the bot.")
+    else:
+        bot.send_message(chat_id=message.chat.id,
+                         text="Sorry, you are not authorized to use this bot.")
+        logger.warning(f"Unauthorized access attempt by user '{username}'.")
 
 
 @bot.message_handler(content_types=['text'])
 def reply(message):
     """ Request """
-    request = message.text
-    logger.info("Received message: %s", request)
-    try:
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt="You: " + request + "\nBot: ",
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.5,
-        ).get("choices")[0].text
-        bot.send_message(chat_id=message.chat.id, text=response)
-        logger.info("Sent response: %s", response)
-    except openai.OpenAIError as ex:
-        logger.error("Error processing message: %s", ex)
+    username = message.from_user.username
+    if username in AUTHORIZED_USERS:
+        request = message.text
+        logger.info("Received message from '%s': %s", username, request)
+
+        try:
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt="You: " + request + "\nBot: ",
+                max_tokens=1024,
+                n=1,
+                stop=None,
+                temperature=0.5,
+            ).get("choices")[0].text
+
+            bot.send_message(chat_id=message.chat.id, text=response)
+            logger.info("Sent response to '%s': %s", username, response)
+        except openai.OpenAIError as ex:
+            logger.error("Error processing message from '%s': %s", username, ex)
+    else:
+        bot.send_message(chat_id=message.chat.id,
+                         text="Sorry, you are not authorized to use this bot.")
+        logger.warning(f"Unauthorized access attempt by user '{username}'.")
 
 
 if __name__ == '__main__':
-    openai.api_key = readfile("openapi.txt")
+    openai.api_key = read_file("openapi.txt")
     logger.info("Bot started")
     bot.polling()
