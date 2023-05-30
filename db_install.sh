99149f9794a775f777ccd5ebcecebce40078219c
Alexey Krylov <elkurhah@gmail.com>
2023-05-30 22:22:02 +0300

    fix error

--- db_install.sh
+++ db_install.sh
@@ -3,6 +3,6 @@ apt-get update
 apt-get -y install postgresql
 
 # Настройка базы данных
-postgres psql -c "CREATE DATABASE telebot;"
-postgres psql -c "CREATE USER myuser WITH PASSWORD 'mypass';"
-postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE telebot TO myuser;"
+sudo -u postgres psql -c "CREATE DATABASE telebot;"
+sudo -u postgres psql -c "CREATE USER myuser WITH PASSWORD 'mypass';"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE telebot TO myuser;"
